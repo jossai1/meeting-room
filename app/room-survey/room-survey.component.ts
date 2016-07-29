6e9c6e5c636410fefdcca564c10d69d146173ce1
jossai1 <buff_jane@hotmail.com>
2016-07-29 14:36:35 +0100

    Update room-survey.component.ts
    Changed qID for question added in pre-prod

--- app/room-survey/room-survey.component.ts
+++ app/room-survey/room-survey.component.ts
@@ -61,7 +61,7 @@ export class RoomSurveyComponent implements OnInit
     {
 
         //for production - set question to the question you want to ask
-        /*change this to the id of whatever question you make in the new db called productionDB*/this.getAQuestion( "578f58f68d0766a34f000393" );
+        /*for pre-PROD ONLY */this.getAQuestion( "579b4769c0c58047a7ceaf1d" );
         var roomName = this.sharedService.getRoomName();
 
         if(roomName == '' )
